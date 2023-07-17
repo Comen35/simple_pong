5a77d6c86ea5c7eb0b7b49d7f49926a1e8793a6b
bahadir <bahadir33@msn.com>
2023-07-17 13:55:57 -0300

    Added ball class

---
+++ lib/ball.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Ball extends StatelessWidget {
+  const Ball({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const double diam = 50;
+    return Container(
+      width: diam,
+      height: diam,
+      decoration: BoxDecoration(
+        color: Colors.amber[400],
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}

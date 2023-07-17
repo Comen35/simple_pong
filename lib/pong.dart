821b574b4c2b5c4b54f232062658317b333c6f73
bahadir <bahadir33@msn.com>
2023-07-17 15:10:55 -0300

    added bat controlling

--- lib/pong.dart
+++ lib/pong.dart
@@ -23,7 +23,7 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   late AnimationController controller;
   Direction vDir = Direction.down;
   Direction hDir = Direction.right;
-  double increment = 5;
+  double increment = 3;
 
   @override
   void initState() {
@@ -35,7 +35,7 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
     );
     animation = Tween<double>(begin: 0, end: 100).animate(controller);
     animation.addListener(() {
-      setState(() {
+      safeSetState(() {
         (hDir == Direction.right) ? posX += increment : posX -= increment;
         (vDir == Direction.down) ? posY += increment : posY -= increment;
       });
@@ -43,6 +43,12 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
     });
     controller.forward();
     super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -62,7 +68,11 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
             ),
             Positioned(
               bottom: 0,
-              child: Bat(width: batWidth, height: batHeight),
+              left: batPosition,
+              child: GestureDetector(
+                onHorizontalDragUpdate: (update) => moveBat(update),
+                child: Bat(width: batWidth, height: batHeight),
+              ),
             ),
           ],
         );
@@ -71,17 +81,38 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   }
 
   void checkBorders() {
+    double diameter = 50;
     if (posX <= 0 && hDir == Direction.left) {
       hDir = Direction.right;
     }
-    if (posX >= width - 50 && hDir == Direction.right) {
+    if (posX >= width - diameter && hDir == Direction.right) {
       hDir = Direction.left;
     }
-    if (posY >= height - 50 && vDir == Direction.down) {
-      vDir = Direction.up;
+    if (posY >= height - diameter - batHeight && vDir == Direction.down) {
+      if (posX >= (batPosition - diameter) &&
+          posX <= (batPosition + batWidth + diameter)) {
+        vDir = Direction.up;
+      } else {
+        controller.stop();
+        dispose();
+      }
     }
     if (posY <= 0 && vDir == Direction.up) {
       vDir = Direction.down;
+    }
+  }
+
+  void moveBat(DragUpdateDetails update) {
+    safeSetState(() {
+      batPosition += update.delta.dx;
+    });
+  }
+
+  void safeSetState(Function function) {
+    if (mounted && controller.isAnimating) {
+      setState(() {
+        function();
+      });
     }
   }
 }

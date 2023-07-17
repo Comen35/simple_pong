a41c5b25c72b9b1ee5f654bc703a0603842f743a
bahadir <bahadir33@msn.com>
2023-07-17 13:59:43 -0300

    Bat class added.

---
+++ lib/bat.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Bat extends StatelessWidget {
+  final double width;
+  final double height;
+  const Bat({super.key, required this.width, required this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: Colors.blue[900],
+      ),
+    );
+  }
+}
